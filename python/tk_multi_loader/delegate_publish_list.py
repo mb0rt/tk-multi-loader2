1eb8d7d3912078d5b75a26361e94d36ea5423eea
Bort <>
2018-05-21 15:12:35 -0300

    added Colors - better description

--- python/tk_multi_loader/delegate_publish_list.py
+++ python/tk_multi_loader/delegate_publish_list.py
@@ -25,11 +25,11 @@ class PublishListWidget(PublishWidget):
     """
     Fixed height thin list item type widget, used for the list mode in the main loader view.
     """
-    
+
     def __init__(self, parent):
         """
         Constructor
-        
+
         :param parent: QT parent object
         """
         PublishWidget.__init__(self, Ui_PublishListWidget, parent)
@@ -37,7 +37,7 @@ class PublishListWidget(PublishWidget):
     def set_text(self, large_text, small_text):
         """
         Populate the lines of text in the widget
-        
+
         :param large_text: Header text as string
         :param small_text: smaller text as string
         """
@@ -48,9 +48,9 @@ class PublishListWidget(PublishWidget):
     def calculate_size():
         """
         Calculates and returns a suitable size for this widget.
-        
+
         :returns: Size of the widget
-        """        
+        """
         return QtCore.QSize(200, 56)
 
 
@@ -63,7 +63,7 @@ class SgPublishListDelegate(PublishDelegate):
         """
         Widget factory as required by base class. The base class will call this
         when a widget is needed and then pass this widget in to the various callbacks.
-        
+
         :param parent: Parent object for the widget
         """
         return PublishListWidget(parent)
@@ -71,7 +71,7 @@ class SgPublishListDelegate(PublishDelegate):
     def _format_folder(self, model_index, widget):
         """
         Formats the associated widget as a folder item.
-        
+
         :param model_index: Model index to process
         :param widget: widget to adjust
         """
@@ -92,7 +92,7 @@ class SgPublishListDelegate(PublishDelegate):
             # this can be a multi link field but also a field like a tags field or a non-entity link type field.
             formatted_values = []
             formatted_types = set()
-            
+
             for v in field_value:
                 if isinstance(v, dict) and "name" in v and "type" in v:
                     # This is a link field
@@ -102,7 +102,7 @@ class SgPublishListDelegate(PublishDelegate):
                         formatted_types.add(v["type"])
                 else:
                     formatted_values.append(str(v))
-            
+
             types = ", ".join(list(formatted_types))
             names = ", ".join(formatted_values)
             main_text = "<b>%s</b><br>%s" % (types, names)
@@ -117,104 +117,53 @@ class SgPublishListDelegate(PublishDelegate):
     def _format_publish(self, model_index, widget):
         """
         Formats the associated widget as a publish item.
-        
+
         :param model_index: Model index to process
         :param widget: widget to adjust
         """
-        
-        # example data:
-        
-        # {'code': 'aaa_00010_F004_C003_0228F8_v000.%04d.dpx',
-        #  'created_at': 1425378837.0,
-        #  'created_by': {'id': 42, 'name': 'Manne Ohrstrom', 'type': 'HumanUser'},
-        #  'created_by.HumanUser.image': 'https://...',
-        #  'description': 'testing testing, 1,2,3',
-        #  'entity': {'id': 1660, 'name': 'aaa_00010', 'type': 'Shot'},
-        #  'id': 1340,
-        #  'image': 'https:...',
-        #  'name': 'aaa_00010, F004_C003_0228F8',
-        #  'path': {'content_type': 'image/dpx',
-        #           'id': 24116,
-        #           'link_type': 'local',
-        #           'local_path': '/mnt/projects...',
-        #           'local_path_linux': '/mnt/projects...',
-        #           'local_path_mac': '/mnt/projects...',
-        #           'local_path_windows': 'z:\\mnt\\projects...',
-        #           'local_storage': {'id': 4,
-        #                             'name': 'primary',
-        #                             'type': 'LocalStorage'},
-        #           'name': 'aaa_00010_F004_C003_0228F8_v000.%04d.dpx',
-        #           'type': 'Attachment',
-        #           'url': 'file:///mnt/projects...'},
-        #  'project': {'id': 289, 'name': 'Climp', 'type': 'Project'},
-        #  'published_file_type': {'id': 53,
-        #                          'name': 'Flame Render',
-        #                          'type': 'PublishedFileType'},
-        #  'task': None,
-        #  'task.Task.content': None,
-        #  'task.Task.due_date': None,
-        #  'task.Task.sg_status_list': None,
-        #  'task_uniqueness': False,
-        #  'type': 'PublishedFile',
-        #  'version': {'id': 6697,
-        #              'name': 'aaa_00010_F004_C003_0228F8_v000',
-        #              'type': 'Version'},
-        #  'version.Version.sg_status_list': 'rev',
-        #  'version_number': 2}
-        
         # Publish Name Version 002
         sg_data = shotgun_model.get_sg_data(model_index)
-        main_text = "<b>%s</b>" % (sg_data.get("name") or "Unnamed")
 
-        version = sg_data.get("version_number")
-        vers_str = "%03d" % version if version is not None else "N/A"
+        item_name    = sg_data.get( 'name' ) or 'Unnamed'
+        variant_name = sg_data.get( 'sg_version_type' ) or 'unVersioned'
+        entity_type  = sg_data.get( 'entity',{} ).get( 'type' )
+        version      = sg_data.get( 'version_number' )
+        publish_type = sg_data.get( 'type' )
+        author       = sg_data.get( 'created_by', {} ).get( 'name' ) or 'Unspecified Author'
+        created_data = sg_data.get( 'created_at' ) or 0
+        publish_type = sg_data.get( 'published_file_type',{} ).get( 'name' ) or "IDK"
+        publish_task = sg_data.get( 'task.Task.step', {}).get( 'name' ) or "Unspecified Task"
 
-        main_text += " Version %s" % vers_str        
+        # -- colors
+        publish_type_color_dict = {'Maya Scene':'#FEAADB', "Alembic Asset":'#FEDFA6', "Maya Material":'#B792E8'}
 
-        # If we are in "show subfolders mode, this line will contain
-        # the entity information (because we are displaying info from several entities 
-        # in a single view. If show subfolders mode is off, the latest description is shown.
-        if self._sub_items_mode:
-            # show items in subfolders mode enabled
-            # get the name of the associated entity
-            
-            main_text += "  ("
-            
-            entity_link = sg_data.get("entity")
-            if entity_link:
-                main_text += "%s <span style='color:#2C93E2'>%s</span>" % (entity_link["type"], entity_link["name"])
+        publish_type_color  = publish_type_color_dict.get( publish_type, '#FFFFFF' )
+        variant_name_color  = '#F6DC1A'
+        task_name_color     = '#2C93E2'
+        item_color          = '#FFFFFF'
 
-            if sg_data.get("task") is not None:
-                main_text += ", Task %s" % sg_data["task"]["name"]
-               
-            main_text += ")"
-        elif sg_data.get("task") is not None:
-            # When not in subfolders mode always show Task info
-            # (similar to the logic in the thumbnail view, but always show)
-            main_text += "  (Task %s)" % sg_data["task"]["name"]
+        # -- to str
+        version_str = "%03d" % version if version is not None else "N/A"
 
-        # Quicktime by John Smith at 2014-02-23 10:34
-        pub_type_str = shotgun_model.get_sanitized_data(model_index, SgLatestPublishModel.PUBLISH_TYPE_NAME_ROLE)            
-        created_unixtime = sg_data.get("created_at") or 0
-        date_str = datetime.datetime.fromtimestamp(created_unixtime).strftime('%Y-%m-%d %H:%M')
-        # created_by is set to None if the user has been deleted.
-        if sg_data.get("created_by") and sg_data["created_by"].get("name"):
-            author_str = sg_data["created_by"].get("name")
-        else:
-            author_str = "Unspecified User"
-        small_text = "<span style='color:#2C93E2'>%s</span> by %s at %s" % (pub_type_str, 
-                                                                            author_str,
-                                                                            date_str)
+        # --- main text
+        if entity_type == 'Shot':
+            main_text = "<span style='color:{variant_color}'>{variant_name}</span> | <B style='color:{task_color}'>{task_name}</B> | <B style='color:{item_color}'>{item_name}</B> | v{version}".format( variant_color=variant_name_color, variant_name=variant_name.upper(), task_color=task_name_color, task_name=publish_task.capitalize(), item_color=item_color, item_name=item_name,  version=version_str )
+
+        elif entity_type == 'Asset':
+            main_text = "<span style='color:{variant_color}'>{variant_name}</span> | <B style='color:{task_color}'>{task_name}</B> | v{version}".format( variant_color=variant_name_color, variant_name=variant_name.upper(), task_color=task_name_color, task_name=publish_task.capitalize(), version=version_str )
+
+        # --- small text
+        date_str = datetime.datetime.fromtimestamp(created_data).strftime('%Y-%m-%d %H:%M')
+
+        small_text = "<B style='color:{type_color}'>{type}</B> by {author} at {date}".format( type_color=publish_type_color, type=publish_type, author=author, date=date_str )
+
         widget.set_text(main_text, small_text)
 
     def sizeHint(self, style_options, model_index):
         """
         Specify the size of the item.
-        
+
         :param style_options: QT style options
         :param model_index: Model item to operate on
         """
         return PublishListWidget.calculate_size()
-
-
-
